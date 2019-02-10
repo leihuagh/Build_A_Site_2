fa04e17a7af82cc2b19f3dc76919d2a0ccffc512
leihua <leihua@masslottery.com>
2019-02-10 08:06:38 -0500

    link list to detail

--- polls/views/_detail.py
+++ polls/views/_detail.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Question, Choice
 
 
 def detail(request, id):
-    return render(request, 'polls/detail.html', {})
+    question = get_object_or_404(Question, id=id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
